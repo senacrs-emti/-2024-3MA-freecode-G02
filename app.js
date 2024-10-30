cf855e3360a34ce766e5a014b1609383d89d9d92
FelipeMYung <102309048+FelipeMYung@users.noreply.github.com>
2024-10-30 10:07:50 -0300

    temperatura add

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-const options = { method: 'GET', headers: { accept: 'application/json' } };
+const options = {method: 'GET', headers: {accept: 'application/json'}};
 const PortoAlegre = { lat: -30.0277, lon: -51.2287 };
 
 fetch(`https://api.tomorrow.io/v4/weather/realtime?location=${PortoAlegre.lat},${PortoAlegre.lon}&apikey=KWRks1B7WhQbOvakyzSBgJFd1eSkEdD7`, options)
   .then(res => res.json())
-  .then(({ data: { values: { temperature } } }) => {
-    console.log('Temperatura:', temperature);
+  .then(res => {
+    let temperatura = res.data.values.temperature;
+    console.log('Temperatura:', temperatura);
+    document.getElementById("temperatura").innerHTML = `Temperatura: ${temperatura} °C`;
   })
   .catch(err => console.error(err));
+
+  
+  
