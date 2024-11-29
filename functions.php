0f4b2d3e75ebf954a4e22637733ecefa24f28584
FelipeMYung <102309048+FelipeMYung@users.noreply.github.com>
2024-11-29 08:02:17 -0300

    banco de dados add

--- functions.php
+++ functions.php
@@ -3,13 +3,30 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "agenda";
+$dbname = "tarefasCalendario";
  
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verifica a conexão
-if ($conn->connect_error) {
-    die("Erro na conexão com o banco de dados: " . $conn->connect_error);
+// Verifica se o formulário foi enviado
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Captura os dados do formulário
+    $dia = $_POST['dia'];
+    $titulo = $_POST['titulo'];
+    $horario = $_POST['horario'];
+    $descricao = $_POST['descricao'];
+
+    // Prepara a consulta SQL para inserção
+    $sql = "INSERT INTO atividades (dia, titulo, horario, descricao) 
+            VALUES ('$dia', '$titulo', '$horario', '$descricao')";
+
+    // Executa a consulta SQL
+    if ($conn->query($sql) === TRUE) {
+        echo "Tarefa adicionada com sucesso!";
+    } else {
+        echo "Erro ao adicionar tarefa: " . $conn->error;
+    }
 }
- 
+
+// Fecha a conexão
+$conn->close();
 ?>
