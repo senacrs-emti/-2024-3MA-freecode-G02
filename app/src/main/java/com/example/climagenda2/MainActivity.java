7efeaa2da30c6827391720da68e64f8ee0a31f02
FelipeMYung <102309048+FelipeMYung@users.noreply.github.com>
2024-12-04 11:47:29 -0300

    arquivos

---
+++ app/src/main/java/com/example/climagenda2/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.climagenda2;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webview);
+
+        // Configurar o WebView
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);  // Habilita JavaScript
+        webSettings.setDomStorageEnabled(true);  // Habilita o armazenamento local (localStorage)
+
+        // Definir WebViewClient para abrir links dentro do WebView
+        webView.setWebViewClient(new WebViewClient());
+
+        // Adicionar a interface do WebApp para comunicação com JavaScript
+        webView.addJavascriptInterface(new WebAppInterface(this), "Android");
+
+        // Carregar o arquivo HTML
+        webView.loadUrl("file:///android_asset/index.html");  // Alterar para o caminho correto
+    }
+}
