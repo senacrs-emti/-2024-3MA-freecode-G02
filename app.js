88bbb6c8b8c2274133d77c87314bbe0bd9cc96b2
FelipeMYung <102309048+FelipeMYung@users.noreply.github.com>
2024-12-06 15:55:39 -0300

    SALVO dados dia 06/12

--- app.js
+++ app.js
@@ -17,32 +17,32 @@ import { obterDiaDaSemanaEDiaDoMes } from './calendario.js'
   for (let i = 1; i <= 7; i++) {
     document.getElementById(`Data-${i}`).innerHTML = obterDiaDaSemanaEDiaDoMes(i - 1, i);
   }
-  const dados2 = fetch('./data.json')  // Aqui você faz o fetch do seu arquivo JSON
+  const dados2 = fetch('./data.json')  
   .then((response) => response.json())
   .then((json) => {
     let prevChuva = diaSelecionado.values.precipitationProbability;
-               // Atualizando a imagem com base no tipo de precipitação
+
                const weatherImage2 = document.getElementById('icone-task');
 
-               // Condicional para escolher a imagem de acordo com o tipo de precipitação
                if (prevChuva > 80) {
-                 weatherImage2.src = './chuva.png';  // Imagem de chuva
-                 weatherImage2.alt = 'Tempo chuvoso';
-                 document.getElementById('info-tempo-task').querySelector('p').innerText = 'Tempo chuvoso';
-               } else if (prevChuva < 80 || prevChuva > 40) {
-                 weatherImage2.src = './nublado.png';  // Imagem de nublado
-                 weatherImage2.alt = 'Tempo nublado';
-                 document.getElementById('info-tempo-task').querySelector('p').innerText = 'Tempo nublado';
-               } else{
-                 weatherImage2.src = './ensolarado.png';  // Imagem de sol
-                 weatherImage2.alt = 'Tempo ensolarado';
-                 document.getElementById('info-tempo-task').querySelector('p').innerText = 'Tempo ensolarado';
-               };
+                weatherImage2.src = './chuva.png'; 
+                weatherImage2.alt = 'Tempo chuvoso';
+                console.log('chuva')
+                document.getElementById('info-tempo2').querySelector('p').innerText = 'Tempo chuvoso';
+              } else if (prevChuva < 80 && prevChuva > 40) {
+                weatherImage2.src = './nublado.png';  
+                weatherImage2.alt = 'Tempo nublado';
+                console.log('nublado')
+                document.getElementById('info-tempo2').querySelector('p').innerText = 'Tempo nublado';
+              } else if(prevChuva < 30){
+                weatherImage2.src = './ensolarado.png';  
+                weatherImage2.alt = 'Tempo ensolarado';
+                console.log('sol')
+                document.getElementById('info-tempo2').querySelector('p').innerText = 'Tempo ensolarado';
+              };
 
   })
-  .catch((error) => {
-    console.error("Erro ao carregar o arquivo JSON:", error);
-  });
+
 /* COLOCAR EM UM BOTÂo
 fetch(`https://api.tomorrow.io/v4/weather/realtime?location=${PortoAlegre.lat},${PortoAlegre.lon}&apikey=KWRks1B7WhQbOvakyzSBgJFd1eSkEdD7`, options)
   .then(res => res.json())
